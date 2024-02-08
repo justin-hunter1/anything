edc29df568231b16e0d4cb6169ba6a97395cf3bb
MauryIV <mauryhughesiv@gmail.com>
2024-02-07 21:56:26 -0700

    event listener prep

---
+++ assets/js/scroll.js
@@ -0,0 +1,307 @@
+var artsValue = document.querySelector("#arts")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var variableValue = document.querySelector("#variable")
+var userCategories = document.querySelector("#user-categories")
+var categoryChoice = document.querySelector("#category-choice")
+var theModal = document.querySelector("#genre-modal")
+var outModal = document.querySelector("#out")
+var exitModal = document.querySelector("#exit")
+var saveModal = document.querySelector("#save")
+var cancelModal = document.querySelector("#cancel")
+
+artsValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "All Arts & Theatre "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Broadway "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Children's Theatre "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Circus & Specialty Acts "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Classical "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Comedy "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Cultural "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Dance "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Espectaculo "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Fashion "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Fine Art "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Magic & Illusion "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Miscellaneous "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Multimedia "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Music "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Opera "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Performance Art "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Puppetry "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Spectacular "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Theatre "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+variableValue.addEventListener("click", function() {
+  userCategories.textContent = userCategories.textContent + "Variety "
+  if (!thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.add("has-background-primary-light", "has-text-primary")
+    return
+  }
+  if (thisoneValue.classList.contains("has-background-primary-light", "has-text-primary")) {
+    thisoneValue.classList.remove("has-background-primary-light", "has-text-primary")
+    return
+  }
+})
+
+categoryChoice.addEventListener("click", function() {
+  theModal.classList.add("is-active")
+})
+
+outModal.addEventListener("click", function() {
+  theModal.classList.remove("is-active")
+})
+
+exitModal.addEventListener("click", function() {
+  theModal.classList.remove("is-active")
+})
+
+saveModal.addEventListener("click", function() {
+  theModal.classList.remove("is-active")
+})
+
+cancelModal.addEventListener("click", function() {
+  theModal.classList.remove("is-active")
+})
+
+// 28<h2 id="category-choice" class="js-modal-trigger" data-target="genre-modal">Genre</h2>
+// 42<div id="genre-modal" class="modal">
+// 43<div id="out" class="modal-background"></div>
+// 47<button id="exit" class="delete" aria-label="close"></button>
+// 73<button id="save" class="button is-primary">Save changes</button>
+// 74<button id="cancel" class="button">Cancel</button>
