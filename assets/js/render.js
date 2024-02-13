9a7f3d63f8e1bab9e5f7dc2be03cab71182f8cf1
Nihsadd <nihsadd@gmail.com>
2024-02-12 20:32:12 -0700

    result cards

--- assets/js/render.js
+++ assets/js/render.js
@@ -1,5 +1,5 @@
 // global variables
-var ResultsElm = document.querySelector("#result-content");
+var resultsElm = document.querySelector("#result-content");
 var eventData = sessionStorage.getItem("Events");
 var data = JSON.parse(eventData);
 var chosenThree = document.querySelector("#user-selection");
@@ -13,16 +13,53 @@ function renderResults() {
   // Creating elements for each event fetched with an image, link and date & time.
   data.forEach(function(object) {
     if (object.dates.status.code !== "cancelled" && object.dates.status.code !== "offsale") {
-      var resultImg = document.createElement("img");
-      var resultDate = document.createElement("p");
-      var resultName = document.createElement("a");
-      resultImg.src = object.images[0].url;
-      resultName.textContent = object.name;
-      resultDate.textContent = dayjs(object.dates.start.dateTime).format("ddd, MMM D, YYYY [at] hh:mm a");
-      resultName.href = object.url;
-      ResultsElm.appendChild(resultImg);
-      ResultsElm.appendChild(resultDate);
-      ResultsElm.appendChild(resultName);
+      var resultCard = document.createElement("div");
+      resultCard.className = "card";
+
+      var cardContent = document.createElement("div");
+      cardContent.className = "card-content";
+
+      var cardMedia = document.createElement("div");
+      cardMedia.className = "media";
+
+      var mediaLeft = document.createElement("div");
+      mediaLeft.className = "media-left";
+
+      var figureClass = document.createElement("figure");
+      figureClass.className = "image is-48x48";
+
+      var mediaContent = document.createElement("div");
+      mediaContent.className = "media-content";
+
+      var pDate = document.createElement("p");
+      pDate.className = "subtitle is-6";
+
+      var aName = document.createElement("a");
+      aName.className = "title is-4";
+
+      var cardImg = document.createElement("img");
+
+      resultsElm.appendChild(resultCard);
+      resultCard.appendChild(cardContent);
+      cardContent.appendChild(cardMedia);
+      cardMedia.appendChild(mediaLeft);
+      mediaLeft.appendChild(figureClass);
+      figureClass.appendChild(cardImg);
+      cardMedia.appendChild(mediaContent);
+      mediaContent.appendChild(pDate);
+      mediaContent.appendChild(aName);
+
+
+      
+      cardImg.src = object.images[0].url;
+      aName.textContent = object.name;
+      pDate.textContent = dayjs(object.dates.start.dateTime).format("ddd, MMM D, YYYY [at] hh:mm a");
+      aName.href = object.url;
+
+
+    //   resultsElm.appendChild(cardImg);
+    //   resultsElm.appendChild(pDate);
+    //   resultsElm.appendChild(aName);
       }
   });
 }
