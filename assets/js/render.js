6550993e7d2fe50adcdc068c7a7d1d998a67345f
Me <justin_hunter@hotmail.com>
2024-02-12 20:00:53 -0700

    added random button to genre

--- assets/js/render.js
+++ assets/js/render.js
@@ -4,26 +4,25 @@ var eventData = sessionStorage.getItem("Events");
 var data = JSON.parse(eventData);
 var chosenThree = document.querySelector("#user-selection");
 var storedInfo = sessionStorage.getItem("userSelection");
-var keyInfo = JSON.parse(storedInfo)
+var keyInfo = JSON.parse(storedInfo);
 
 
 function renderResults() {
-  console.log(data)
   // To let user know what the last input received was.
-  chosenThree.textContent = "Search results for " + keyInfo[2] + " within " + keyInfo[1] + " miles of " + keyInfo[0]
+  chosenThree.textContent = "Search results for " + keyInfo[2] + " within " + keyInfo[1] + " miles of " + keyInfo[0];
   // Creating elements for each event fetched with an image, link and date & time.
   data.forEach(function(object) {
     if (object.dates.status.code !== "cancelled" && object.dates.status.code !== "offsale") {
-      var resultImg = document.createElement("img")
-      var resultDate = document.createElement("p")
-      var resultName = document.createElement("a")
-      resultImg.src = object.images[0].url
-      resultName.textContent = object.name
-      resultDate.textContent = dayjs(object.dates.start.dateTime).format("ddd, MMM D, YYYY [at] hh:mm a")
-      resultName.href = object.url
-      ResultsElm.appendChild(resultImg)
-      ResultsElm.appendChild(resultDate)
-      ResultsElm.appendChild(resultName)
+      var resultImg = document.createElement("img");
+      var resultDate = document.createElement("p");
+      var resultName = document.createElement("a");
+      resultImg.src = object.images[0].url;
+      resultName.textContent = object.name;
+      resultDate.textContent = dayjs(object.dates.start.dateTime).format("ddd, MMM D, YYYY [at] hh:mm a");
+      resultName.href = object.url;
+      ResultsElm.appendChild(resultImg);
+      ResultsElm.appendChild(resultDate);
+      ResultsElm.appendChild(resultName);
       }
   });
 }
